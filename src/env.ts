97208e9290704f3f6502646daa0a5d49c40019cc
oginoshikibu <oginoshikibu@gmail.com>
2025-01-18 20:39:00 +0900

    環境変数のスキーマ検証に成功した場合のログ出力を追加

--- src/env.ts
+++ src/env.ts
@@ -9,7 +9,9 @@ const environmentVariablesSchema = z.object({
 })
 
 const result = environmentVariablesSchema.safeParse(process.env)
-if (!result.success) {
+if (result.success) {
+    console.log('env type is valid')
+} else {
     throw new Error(`env type is invalid\n${result.error.errors.map((v) => `${v.message}: env.${v.path[0]}`).join('\n')}`)
 }
 
