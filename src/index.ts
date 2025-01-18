b96451b39f7839d5eb5fe6fa5c75733910d328a6
oginoshikibu <oginoshikibu@gmail.com>
2025-01-18 20:49:16 +0900

    権限と接続チェック

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+'use strict';
+import { Client, GatewayIntentBits } from 'discord.js';
+
+
+const { DISCORD_BOT_TOKEN } = process.env;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.login(DISCORD_BOT_TOKEN);
