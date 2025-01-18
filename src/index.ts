c2ad0d7936de256ccab689369a848cd225d1db0f
oginoshikibu <oginoshikibu@gmail.com>
2025-01-18 20:54:23 +0900

    クライアントの準備完了時にハンドラーを追加

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 'use strict';
-import { Client, GatewayIntentBits } from 'discord.js';
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+import { clientReadyHandler } from './eventHandlers/clientReadyHandler';
 
 
 const { DISCORD_BOT_TOKEN } = process.env;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.on(Events.ClientReady, clientReadyHandler);
 
 client.login(DISCORD_BOT_TOKEN);
