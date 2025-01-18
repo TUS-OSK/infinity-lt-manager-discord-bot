66981ccbea4ba06c0fcc196dc01a22101cf18866
oginoshikibu <oginoshikibu@gmail.com>
2025-01-18 20:53:55 +0900

    typo

--- src/eventHandlers/clientReadyHandler.ts
+++ src/eventHandlers/clientReadyHandler.ts
@@ -1,6 +1,6 @@
 import type { Client } from "discord.js";
 
-export const clientRedyHandler = async (client: Client) => {
+export const clientReadyHandler = async (client: Client) => {
     console.log('clientReadyHandler start');
     console.log(`Logged in as ${client.user?.tag ?? 'unknown user'} at ${new Date().toISOString()}`);
     console.log('clientReadyHandler end');
