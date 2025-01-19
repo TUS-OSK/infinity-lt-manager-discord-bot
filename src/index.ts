b9bc9a9ca0c9478e06c25a5a6e60873137508a34
oginoshikibu <oginoshikibu@gmail.com>
2025-01-19 11:32:11 +0900

    clientにinteractionを登録

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { Client, Events, GatewayIntentBits } from 'discord.js';
 import { clientReadyHandler } from './eventHandlers/clientReadyHandler';
+import { interactionCreateHandler } from './eventHandlers/interactionCreateHandler';
 
 
 const { DISCORD_BOT_TOKEN } = process.env;
@@ -7,5 +8,6 @@ const { DISCORD_BOT_TOKEN } = process.env;
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 client.on(Events.ClientReady, clientReadyHandler);
+client.on(Events.InteractionCreate, interactionCreateHandler);
 
 client.login(DISCORD_BOT_TOKEN);
