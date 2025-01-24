0d89a97411c3b541bee0b90522e2cea280184cb9
oginoshikibu <oginoshikibu@gmail.com>
2025-01-24 16:54:47 +0900

    次回のLT通知コマンドに管理者制限を追加

--- src/commands/notifyNextLTsCommand.ts
+++ src/commands/notifyNextLTsCommand.ts
@@ -3,17 +3,23 @@ import { SlashCommandBuilder } from '@discordjs/builders';
 import type { Command } from '../types';
 import { notifyNextLTsByCommand } from '../services/LTNotificationService';
 
+const { ADMIN_USER_ID } = process.env;
+
 export const notifyNextLTsCommand: Command = {
     data: new SlashCommandBuilder()
-    .setName('next-lt')
-    .setDescription('次回のLTを通知します。')
-    .addIntegerOption(option => option.setName('limit').setDescription('発表者数（default: 10）').setRequired(false)),
+        .setName('next-lt')
+        .setDescription('（LT会管理者のみ実行可能）次回のLTを通知します。')
+        .addIntegerOption(option => option.setName('limit').setDescription('発表者数（default: 10）').setRequired(false)),
 
     isThisCommand: function (interaction) {
         return interaction.commandName === this.data.name;
     },
 
     execute: async function (interaction) {
+        if (interaction.user.id !== ADMIN_USER_ID) {
+            await interaction.reply({ content: 'このコマンドは特定の管理者のみ実行可能です。', flags: MessageFlags.Ephemeral });
+            return;
+        }
         await interaction.deferReply({ flags: MessageFlags.Ephemeral });
         await notifyNextLTsByCommand(interaction);
     }
