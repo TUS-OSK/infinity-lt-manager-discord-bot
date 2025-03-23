ed0c8618ac01fa32ca3888d4b491c5a7b3747339
oginoshikibu <oginoshikibu@gmail.com>
2025-03-23 15:18:49 +0900

    MoveToFrontLTButtonをボタンリストに追加

--- src/buttons/index.ts
+++ src/buttons/index.ts
@@ -3,6 +3,7 @@ import { deleteLTButton } from './deleteLTButton';
 import { moveNextLTButton } from './moveNextLTButton';
 import { readyLTButton } from './readyLTButton';
 import { unreadyLTButton } from './unreadyLTButtons';
+import { MoveToFrontLTButton } from './moveToFrontLTButton';
 
 
 const buttons: Button[] = [
@@ -10,6 +11,7 @@ const buttons: Button[] = [
     readyLTButton,
     unreadyLTButton,
     moveNextLTButton,
+    MoveToFrontLTButton
 ];
 
 export default buttons;
